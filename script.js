2bb9ea191fe836261c5c7b68e1b71ef4ff6752b9
Ryan King <ryan.h.king@outlook.com>
2024-06-14 21:35:17 -0400

    adjusted logic

--- script.js
+++ script.js
@@ -15,12 +15,79 @@ function getComputerChoice(){
     }
 }
 
-
+// Player Logic
 const choices = ["rock", "paper", "scissors"];
 
 function getPlayerChoice(){
     
-    let choice = prompt("Please choose rock, paper, or scissors. ")
-        
+    let choice = prompt("Please choose rock, paper, or scissors. ").toLowerCase();
+    if (choice === "rock" || choice ==="paper" || choice === "scissors") {
+        return choice 
+    }    else {
+        console.log("Invalid choice");
+        return;
+    }
 }
-console.log(getPlayerChoice())
+
+//Initialize score 
+let playerScore = 0;
+let computerScore = 0;
+
+
+
+//Logic to play a single round 
+
+function playRound(playerChoice, computerChoice){
+    if (playerChoice === computerChoice) {
+        console.log("It's a Tie! " + "Player chose: " + playerChoice + " and Computer chose: " + computerChoice)
+    } else if (playerChoice == "rock" && computerChoice == "paper") {
+        console.log("Player won!")
+        playerScore++;
+        console.log("Player Score: " + playerScore + " Computer Score: " + computerScore);
+    } else if (playerChoice == "rock" && computerChoice == "scissors") {
+        console.log("Computer Wins!")
+        computerScore++;
+        console.log("Player Score: " + playerScore + " Computer Score: " + computerScore);
+    } if (playerChoice == "paper" && computerChoice == "rock") {
+        console.log("Player Wins!")
+        computerScore++;
+        console.log("Computer Score: " + computerScore + " Player Score: "+ playerScore);
+    } else if (playerChoice == "paper" && computerChoice == "scissors"){
+        console.log("Computer Wins!")
+        computerScore++;
+        console.log("Player Score: " + playerScore + " Computer Score: " + computerScore);
+    } if (playerChoice == "scissors" && computerChoice == "rock") {
+        console.log("Computer Wins!")
+        computerScore++;
+        console.log("Player Score: " + playerScore + " Computer Score: " + computerScore);
+    } else if (playerChoice == "scissors" && computerChoice == "paper") {
+        console.log("Player Wins!")
+        playerScore++;
+        console.log("Player Score: " + playerScore + " Computer Score: " + computerScore);
+    } else {
+        return
+    }
+}
+
+//Play 5 rounds of the game 
+
+function playGame(){
+
+for (let i = 0; i < 5; i += 1){
+    console.log(`Round ${Math.round(i+i)}`);
+
+const playerChoice = getPlayerChoice();
+const computerChoice = getComputerChoice();
+
+playRound(playerChoice, computerChoice);
+}
+    if (playerScore<computerScore){
+        console.log("Copmuter Wins!")
+    } else if (playerScore < computerScore){
+        console.log("Player Wins")
+    } else {
+        console.log("It's a tie!")
+    }
+}
+
+playGame();
